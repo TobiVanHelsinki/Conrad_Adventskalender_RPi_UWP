553236bdca2fca470c80bbbc9d700b453011b4c2
Tobias Pabst <TobivanHelsinki@live.de>
2017-12-02 15:07:26 +0100

    init code commit with day 1

---
+++ Conrad_RPi/Conrad_RPi/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Conrad_RPi
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += Page_Loaded;
+        }
+
+        #region GPIO Test
+        GpioPin pin;
+        GpioController gpio;
+        private readonly int LEDPIN = 4;
+
+        private void DayAction01(object sender, RoutedEventArgs e)
+        {
+            pin.Write(LEDSwitch.IsOn ? GpioPinValue.High : GpioPinValue.Low);
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            // load default GpioController
+            gpio = GpioController.GetDefault();
+
+            //set the connection to the gpio pin
+            pin = gpio.OpenPin(LEDPIN);
+
+            //set Value for the Pin to High = LED off
+            pin.Write(GpioPinValue.High);
+
+            //config the pin for output
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+        #endregion
+    }
+}
